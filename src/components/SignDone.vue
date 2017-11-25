f78598b3982ca41201ada01447c9476b497f370d
Little-God <377954252@qq.com>
2017-11-25 18:07:09 +0800

    add this lit

--- src/components/SignDone.vue
+++ src/components/SignDone.vue
@@ -5,11 +5,26 @@
       <p><router-link to="/" class="go">积分兑换</router-link></p>
       <p class="continuity">已连续签到<span>{{daynum}}</span>天</p>
       <ul class="goods_list">
-        <li v-for="(item,index) in goods" :class="{'active':index%2!=1}">
-          <p class="good_img"><img src="../assets/img/mai.jpg" alt=""></p>
-          <p class="good_name"><span class="tag">{{item.tag}}</span>{{item.good_name}}</p>
-          <p class="use">{{item.good_use}}</p>
-          <p class="money">价值{{item.good_money}}元</p>
+        <li>
+          <div class="good_img">
+            <img src="../assets/img/mai.jpg" alt="">
+            <p class="good_name">连续签到1天可以领取</p>
+          </div>
+          <div class="btn_qu"><span class="btnqu active">待领取</span></div>
+        </li>
+        <li>
+          <div class="good_img">
+            <img src="../assets/img/mai.jpg" alt="">
+            <p class="good_name">连续签到1天可以领取</p>
+          </div>
+          <div class="btn_qu"><span class="btnqu">待领取</span></div>
+        </li>
+        <li>
+          <div class="good_img">
+            <img src="../assets/img/mai.jpg" alt="">
+            <p class="good_name">连续签到1天可以领取</p>
+          </div>
+          <div class="btn_qu"><span class="btnqu">待领取</span></div>
         </li>
       </ul>
     </section>
@@ -41,6 +56,7 @@
   }
   p{
     text-align: center;
+    margin:0;
   }
   .total{
     width: 70%;
@@ -53,6 +69,7 @@
     font-weight: 500;
     text-align: center;
     padding-top:.2rem;
+    margin-bottom: .15rem;
   }
   .total span{
     font-size: .22rem;
@@ -76,5 +93,43 @@
   .link a{
     color: #fff;
     text-decoration: underline;
+  }
+  .goods_list{
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    margin-top:.15rem;
+  }
+  .goods_list li{
+    width:46%;
+  }
+  .good_img img{
+    width:100%;
+  }
+  .good_img{
+    margin:0 0 .1rem .21rem;
+    border:1px solid rgba(0,0,0,0.2);
+  }
+  .good_name{
+    font-size: .09rem;
+    color: #fff800;
+    padding:.03rem 0;
+    text-align: center;
+  }
+  .btn_qu{
+    padding-left:.2rem;
+    text-align: center;
+    margin-bottom: .2rem;
+  }
+  .btnqu{
+    padding:.03rem .14rem;
+    background: #fffc88;
+    border-radius: 20px;
+    color: #866f6b;
+    font-weight: 700;
+  }
+  .btnqu.active{
+    background: #ddd;
+    color: #9a9a9a;
   }
 </style>
